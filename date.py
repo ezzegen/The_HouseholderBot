39c87b6f47172a8f1901ba52b56b35604bce3a74
ezzegen <nikaveronichka@yandex.ru>
2022-08-01 00:33:58 +0400

    Created function

--- date.py
+++ date.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://my-calend.ru/day-and-number-today')
-soup = BeautifulSoup(response.text, 'html.parser')
-split_lst = soup.get_text().split('Праздники сегодня')[0].split('Какое сегодня число и день недели ')
-today = split_lst[-1]
+
+def date(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    split_lst = soup.get_text().split('Праздники сегодня')[0].split('Какое сегодня число и день недели ')
+    return split_lst[-1]
+
+
+today = date('https://my-calend.ru/day-and-number-today')
+
+if __name__ == '__main__':
+    date('https://my-calend.ru/day-and-number-today')
