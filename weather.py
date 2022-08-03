65e8845092d85d76f9db25e68deb585a2bc956ab
ezzegen <nikaveronichka@yandex.ru>
2022-08-03 11:14:04 +0400

    Created function, documentation

--- weather.py
+++ weather.py
@@ -1,11 +1,26 @@
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get('https://world-weather.ru/pogoda/russia/samara/')
-content = response.text
-pars = content.split('<span class="dw-into">')[1].split('<span id="open-desc-weather">')[0]
-weather_pars = pars.split()
-join_lst = ' '.join(weather_pars)
-if 'Облачно' or 'Ясно' in weather_pars:
-    weather_smr = f'{join_lst}\n\nОтличная погода для пробежки или, может, для прогулки... Хотя лучше на дачу!\U0001F493'
-else:
-    weather_smr = f'{join_lst}\n\nХммм... А я думаю, почему меня в сон клонит...'
+
+def weather(url):
+    """
+    Find out the weather in Samara
+    :param: url(str)
+    :return: str
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    find_weather = soup.find_all('span', class_="dw-into")
+    clear_weather = [c.text for c in find_weather][0].split('погода')[1].split('Подробнее')[0]
+    if 'Облачно' or 'Ясно' in clear_weather:
+        return f'{clear_weather}\n\nОтличная погода для пробежки или,' \
+                      f' может, для прогулки... Хотя лучше на дачу!\U0001F493'
+    else:
+        return f'{clear_weather}\n\nХммм... А я думаю,' \
+                      f' почему меня в сон клонит...'
+
+
+weather_smr = weather('https://world-weather.ru/pogoda/russia/samara/')
+
+if __name__ == 'main':
+    weather('https://world-weather.ru/pogoda/russia/samara/')
