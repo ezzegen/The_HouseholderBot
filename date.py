c3b9d2088dd881ac36a531c7f59e976d3cb29535
ezzegen <nikaveronichka@yandex.ru>
2022-08-17 23:28:35 +0400

    Error handler

--- date.py
+++ date.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
-def date(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    split_lst = soup.get_text().split('Праздники сегодня')[0].split('Какое сегодня число и день недели ')
-    return split_lst[-1]
+def date(url, retry=5):
+    """
+    Date and day of the week now
+    :param: url(str), retry(int)
+    :return: str
+    """
+    try:
+        response = requests.get(url)
+    except Exception:
+        time.sleep(3)
+        if retry:
+            return date(url, retry=retry-1)
+        else:
+            return 'Сервис временно недоступен, но можно глянуть в календарь на телефоне)).'
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        split_lst = soup.get_text().split('Праздники сегодня')[0].split('Какое сегодня число и день недели ')
+        return split_lst[-1]
 
 
 today = date('https://my-calend.ru/day-and-number-today')
--- weather.py
+++ weather.py
@@ -1,22 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
-def weather(url):
+def weather(url, retry=5):
     """
     Find out the weather in Samara
-    :param: url(str)
+    :param: url(str), retry(int)
     :return: str
     """
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    find_weather = soup.find_all('span', class_="dw-into")
-    clear_weather = [c.text for c in find_weather][0].split('погода')[1].split('Подробнее')[0]
-    if 'Облачно' or 'Ясно' in clear_weather:
-        return f'{clear_weather}\n\nОтличная погода для пробежки или,' \
-                      f' может, для прогулки... Хотя лучше на дачу!\U0001F493'
+    try:
+        response = requests.get(url)
+    except Exception:
+        time.sleep(3)
+        if retry:
+            return weather(url, retry=retry-1)
+        else:
+            return 'Сервис временно недоступен'
     else:
-        return f'{clear_weather}\n\nХммм... А я думаю,' \
+        soup = BeautifulSoup(response.text, 'html.parser')
+        find_weather = soup.find_all('span', class_="dw-into")
+        clear_weather = [c.text for c in find_weather][0].split('погода')[1].split('Подробнее')[0]
+        if 'Облачно' or 'Ясно' in clear_weather:
+            return f'{clear_weather}\n\nОтличная погода для пробежки или,' \
+                      f' может, для прогулки... Хотя лучше на дачу!\U0001F493'
+        else:
+            return f'{clear_weather}\n\nХммм... А я думаю,' \
                       f' почему меня в сон клонит...'
 
 
