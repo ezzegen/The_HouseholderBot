d542acce8461f2c06d63bf8a1acd2d82e2619103
ezzegen <nikaveronichka@yandex.ru>
2022-08-17 22:47:38 +0400

    Error Handler

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import telebot
 from config import token
 import threading
+import time
+import logging
 
 bot = telebot.TeleBot(token)
-
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 import handlers
 import callbacks
 from notifications import notifications, users_id
 
-t1 = threading.Thread(target=bot.polling)
-t2 = threading.Thread(target=notifications, args=(users_id,))
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+while True:
+    try:
+        t1 = threading.Thread(target=bot.polling(non_stop=True))
+        t2 = threading.Thread(target=notifications, args=(users_id,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+    except Exception as e:
+        logger.error(e)
+        time.sleep(15)
